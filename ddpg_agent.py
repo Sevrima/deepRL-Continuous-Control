2ab62912923b1a8e795e0c636cf7b149baf9a2e6
Amir Fotovvat <afotovat@ymail.com>
2020-06-27 22:14:23 -0600

    completing agent

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -2,25 +2,41 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torch.optim as optim
 from model import Actor, Critic
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.90            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate
+LR_actor = 5e-4               
+LR_critic = 5e-4
 UPDATE_EVERY = 4        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-class ddpg_agent:
+class Agent:
 
-    def __init__(self):
-        pass
+    def __init__(self, state_size, action_size, seed):
+        """Initializing the agent"""
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = seed
 
-    def act(self):
+        self.actor_local = Actor(state_size, action_size)
+        self.actor_target = Actor(state_size, action_size)
+        self.actor_optim = optim.Adam(self.actor_local.parameters, lr = LR_actor)
+
+        self.critic_local = Critic(state_size, action_size)
+        self.critic_target = Critic(state_size, action_size)
+        self.critic_optim = optim.Adam(self.actor_local.parameters, lr = LR_critic)
+
+
+        self.noise = OUNoise(action_size, seed = self.seed)
+        self.buffer = ReplayBuffer(aciton_size, BUFFER_SIZE, BATCH_SIZE, seed = self.seed) 
+
+    def act(self, state):
         pass
 
     def step(sefl):
@@ -32,6 +48,31 @@ class ddpg_agent:
     def soft_update(self):
         pass
 
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.size = size
+        self.seed = random.seed(seed)
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+
+        # Thanks to Hiu C. for this tip, this really helped get the learning up to the desired levels
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.standard_normal(self.size)
+
+        self.state = x + dx
+        return self.state
 
 class ReplayBuffer:
     """Fixed-size buffer to store experience tuples."""
